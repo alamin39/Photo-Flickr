4c9b00160abe781a2a7e1333b0b223090ec0387a
Al-Amin <alamin61a@gmail.com>
2023-04-24 12:35:21 +0900

    adding pinch gesture

--- Photo Flickr/Photo Flickr/ViewControllers/DetailsViewController.swift
+++ Photo Flickr/Photo Flickr/ViewControllers/DetailsViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 import Kingfisher
 
 class DetailsViewController: UIViewController {
-
+    
     @IBOutlet weak var photoView: UIImageView!
     var imageUrl: URL? = nil
     
@@ -17,7 +17,24 @@ class DetailsViewController: UIViewController {
         super.viewDidLoad()
         title = "Details"
         if let imageUrl = imageUrl {
-           photoView.kf.setImage(with: imageUrl)
+            photoView.kf.setImage(with: imageUrl)
+            photoView.enableZoom()
         }
+    }
+}
+
+extension UIImageView {
+    func enableZoom() {
+        let pinchGesture = UIPinchGestureRecognizer(target: self, action: #selector(startZooming(_:)))
+        isUserInteractionEnabled = true
+        addGestureRecognizer(pinchGesture)
+    }
+    
+    @objc
+    private func startZooming(_ sender: UIPinchGestureRecognizer) {
+        let scaleResult = sender.view?.transform.scaledBy(x: sender.scale, y: sender.scale)
+        guard let scale = scaleResult, scale.a > 1, scale.d > 1 else { return }
+        sender.view?.transform = scale
+        sender.scale = 1
     }
 }
