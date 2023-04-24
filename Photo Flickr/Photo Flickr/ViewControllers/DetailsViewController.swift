3dfe8102b675cfda45f29839a84f0762c9ffba1e
Al-Amin <alamin61a@gmail.com>
2023-04-24 14:24:37 +0900

    adding UI, Unit test cases and share feature

--- Photo Flickr/Photo Flickr/ViewControllers/DetailsViewController.swift
+++ Photo Flickr/Photo Flickr/ViewControllers/DetailsViewController.swift
@@ -21,20 +21,12 @@ class DetailsViewController: UIViewController {
             photoView.enableZoom()
         }
     }
-}
-
-extension UIImageView {
-    func enableZoom() {
-        let pinchGesture = UIPinchGestureRecognizer(target: self, action: #selector(startZooming(_:)))
-        isUserInteractionEnabled = true
-        addGestureRecognizer(pinchGesture)
-    }
     
-    @objc
-    private func startZooming(_ sender: UIPinchGestureRecognizer) {
-        let scaleResult = sender.view?.transform.scaledBy(x: sender.scale, y: sender.scale)
-        guard let scale = scaleResult, scale.a > 1, scale.d > 1 else { return }
-        sender.view?.transform = scale
-        sender.scale = 1
+    @IBAction func shareImageButton(_ sender: UIButton) {
+        let image = photoView.image
+        let imageToShare = [ image! ]
+        let activityViewController = UIActivityViewController(activityItems: imageToShare, applicationActivities: nil)
+        activityViewController.popoverPresentationController?.sourceView = self.view
+        present(activityViewController, animated: true)
     }
 }
--- Photo Flickr/Photo FlickrUITests/Photo_FlickrUITests.swift
+++ Photo Flickr/Photo FlickrUITests/Photo_FlickrUITests.swift
@@ -8,28 +8,40 @@
 import XCTest
 
 final class Photo_FlickrUITests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+    
+    var app: XCUIApplication!
+    var searchField: XCUIElement!
+    
+    override func setUp() {
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
+        
+        app = XCUIApplication()
         app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        searchField = app.searchFields.firstMatch
     }
-
+    
+    override func tearDown() {
+        app = nil
+    }
+    
+    func test_SearchField_is_enabled() {
+        let navigationBar = app.navigationBars["Photos"]
+        
+        let searchField = navigationBar.searchFields["Search photos"]
+        XCTAssert(searchField.isEnabled)
+    }
+    
+    func test_SearchField_exists() {
+        XCTAssertTrue(searchField.exists, "Search field not found")
+    }
+    
+    func test_SearchField_NotEmpty() {
+        searchField.tap()
+        searchField.typeText("Cat")
+        
+        XCTAssertNotNil(searchField.title)
+    }
+    
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
             // This measures how long it takes to launch your application.
